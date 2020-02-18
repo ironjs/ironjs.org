f7c1f8ef25f90f1f2bb3cf8cc0db1832e374203b
Sam Holmes <sam.lb.holmes@gmail.com>
2020-02-17 21:24:06 -0800

    Initial site

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-document.getElementById('root').textContent = 'Coming soon'
+import { e, jarvis } from './lib.js'
+
+function App() {
+	return e.div.wrapper(
+		e.header(
+			e.div.container(
+				{ style: { textAlign: 'center' } },
+				e.div.logo(
+					e.img({
+						src: '/assets/logo.svg',
+						style: { maxWidth: '100%' },
+					})
+				),
+				e.h1('A Modern Applications Framework')
+			)
+		),
+		e.content(
+			e.div.container(
+				e.p(
+					e.a.button({ href: 'http://github.com/samholmes/iron' }, 'Docs'),
+					e.a.button({ href: 'http://github.com/samholmes/iron' }, 'GitHub')
+				)
+			)
+		),
+		e.footer(e.div.container(e.p(e._unsafe('&copy;'), ' Copyright')))
+	)
+}
+
+jarvis.render(App(), document.getElementById('root'))
---
+++ lib.js
@@ -0,0 +1,1 @@
+export * from 'https://cdn.jsdelivr.net/gh/samholmes/iron@0.0.2-alpha/index.js'
